dbd4c4df78d9fd969effe3237f82d1acfc1198a3
chenyifan <chenyifan@localhost>
2022-11-17 12:45:14 -0600

    First Try

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,65 @@
 package studio8;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Objects;
+
 public class Date {
+	private int month; // all months have 31 days
+	private int day;
+	private int year;
+	private boolean isHoliday;
+	
+	/**
+	 * 
+	 * @param tempM
+	 * @param tempD
+	 * @param tempY
+	 * @param isH
+	 */
+	public Date(int tempM, int tempD, int tempY, boolean isH) {
+		month = tempM;
+		day = tempD;
+		year = tempY;
+		isHoliday = isH;
+	}
 
+    @Override
+	public String toString() {
+		return "Date [month=" + month + ", day=" + day + ", year=" + year + ", isHoliday=" + isHoliday + "]";
+	}
 
-    public static void main(String[] args) {
+	@Override
+	public int hashCode() {
+		return Objects.hash(day, isHoliday, month, year);
+	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		return day == other.day && isHoliday == other.isHoliday && month == other.month && year == other.year;
+	}
+
+	public static void main(String[] args) {
+		LinkedList<Date> list = new LinkedList<Date>();
+		Date d1 = new Date(1,1,2020,true);
+		Date d2 = new Date(1,1,2020,true);
+		list.add(d1);
+		list.add(d2);
+		list.add(d1);
+		System.out.println(list);
+		
+		HashSet<Date> set = new HashSet<Date>();
+		set.add(d1);
+		set.add(d2);
+		set.add(d1);
+		System.out.println(set);
     }
 
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -2,6 +2,24 @@ package studio8;
 
 
 public class Time {
+	private int hour;
+	private int minute;
+	private boolean twelveFormat;
+	
+	public Time(int tempH, int tempM, boolean tempT) {
+		hour = tempH;
+		minute = tempM;
+		twelveFormat = tempT;
+	}
+	
+	
+
+	@Override
+	public String toString() {
+		return "Time [hour=" + hour + ", minute=" + minute + ", twelveFormat=" + twelveFormat + "]";
+	}
+
+
 
 	public static void main(String[] args) {
  
